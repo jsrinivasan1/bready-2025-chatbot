698fe6a63293714444175f28189e76e80cea34c2
Jayashree Srinivasan <jsrinivasan1@worldbank.org>
2026-01-10 11:47:59 -0500

    Add ping function for testing

---
+++ netlify/functions/ping.js
@@ -0,0 +1,7 @@
+exports.handler = async () => {
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "application/json; charset=utf-8" },
+    body: JSON.stringify({ ok: true })
+  };
+};
